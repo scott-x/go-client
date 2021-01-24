56a50dca23d116c34ac16240af1be80dff0618b3
Koichi Shiraishi <zchee.io@gmail.com>
2021-01-24 21:41:20 +0900

    nvim: fix walkErr to ignore IsNotExist error

--- nvim/nvim_test.go
+++ nvim/nvim_test.go
@@ -71,7 +71,7 @@ func newChildProcess(tb testing.TB) (v *Nvim, cleanup func()) {
 			}
 
 			return nil
-		}); walkErr != nil {
+		}); walkErr != nil && !os.IsNotExist(err) {
 			tb.Fatal(fmt.Errorf("walkErr: %w", errors.Unwrap(walkErr)))
 		}
 	}
